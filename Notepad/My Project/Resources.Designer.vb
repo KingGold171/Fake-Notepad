d0bc67d2ebd07441b280a91c21591843bd41fef4
KingGold171 <kinggold171@gmail.com>
2016-07-29 15:28:52 +0100

    Taskbar icon updated - compiled

--- Notepad/My Project/Resources.Designer.vb
+++ Notepad/My Project/Resources.Designer.vb
@@ -69,15 +69,5 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Icon)
             End Get
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property notepad1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("notepad1", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
